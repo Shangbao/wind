4b8490b2a0f8fe58ef48cda08eab23e061c6279b
xiaoyongAaron <494309145@qq.com>
2017-04-19 19:44:27 +0800

    first commit

---
+++ wind-core/src/main/java/com/wind/carMessage/model/ByscVO.java
@@ -0,0 +1,7 @@
+package com.wind.carMessage.model;
+
+/**
+ * Created by Administrator on 2016/8/10.
+ */
+public class ByscVO {
+}
---
+++ wind-webapp/src/main/java/com/wind/common/Utils.java
@@ -0,0 +1,7 @@
+package com.wind.common;
+
+/**
+ * Created by Administrator on 2016/8/10.
+ */
+public class Utils {
+}
